338a82625f15e82dab20499b94c613a31616140e
pekellogg <pekellogg@hotmail.com>
2022-12-20 19:22:07 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import AddUser from "./components/users/AddUser";
+import UsersList from "./components/users/UsersList";
+
+function App() {
+
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (userName, userAge) => {
+    setUsersList((previousUsersList) => {
+      return [
+        ...previousUsersList, { 
+          id: Math.random().toString(),
+          name: userName,
+          age: userAge
+        }];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+
+};
+
+export default App;
